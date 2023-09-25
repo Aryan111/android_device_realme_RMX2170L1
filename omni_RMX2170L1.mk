4b9b931a83c653211a79288ac0844c9d7ba1f9ba
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-09-25 19:28:14 +0200

    RMX2170L1: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: realme Model: RMX2170 Script version: 3.0.0

---
+++ omni_RMX2170L1.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from RMX2170L1 device
+$(call inherit-product, device/realme/RMX2170L1/device.mk)
+
+PRODUCT_DEVICE := RMX2170L1
+PRODUCT_NAME := omni_RMX2170L1
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX2170
+PRODUCT_MANUFACTURER := realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX2170-user 12 SKQ1.210216.001 Q.202212131525 release-keys"
+
+BUILD_FINGERPRINT := realme/RMX2170/RMX2170L1:12/SKQ1.210216.001/Q.202212131525:user/release-keys
