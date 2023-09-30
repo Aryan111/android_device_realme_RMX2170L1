f2c8e062246c2356cb9e0fe223c19158983ee527
Aryan111 <45229110+Aryan111@users.noreply.github.com>
2023-09-30 22:10:07 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,19 @@
 #
 
 LOCAL_PATH := device/realme/RMX2170L1
+
+# Dynamic partition stuff
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Api
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# Qcom common decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt\
+    qcom_decrypt_fbe
+
+# fastboot/d hal
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
